89122d9599e0a85e64db3becf707fab2cd76e183
Haicang <33958283+Haicang@users.noreply.github.com>
2020-01-11 15:27:03 +0800

    Update for higher vision of PyTorch.
    - Replace `Variable.data.xxx` as `Tensor.xxx`
    - Replace `FloatTensor` as default `Tensor`, which is set as `FloatTensor` as default, but user could choice another type.

--- pygcn/layers.py
+++ pygcn/layers.py
@@ -11,22 +11,22 @@ class GraphConvolution(Module):
     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
     """
 
-    def __init__(self, in_features, out_features, bias=True):
+    def __init__(self, in_features:int, out_features:int, bias=True):
         super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        self.weight = Parameter(torch.Tensor(in_features, out_features))
         if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))
+            self.bias = Parameter(torch.Tensor(out_features))
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
 
     def reset_parameters(self):
         stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
+        self.weight.uniform_(-stdv, stdv)
         if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
+            self.bias.uniform_(-stdv, stdv)
 
     def forward(self, input, adj):
         support = torch.mm(input, self.weight)
